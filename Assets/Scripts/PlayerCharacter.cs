9241efa6fe9b18af542f6b02d5dc7fa05a908dd4
EthanB976 - Lab 2 <136556095+EthanB976@users.noreply.github.com>
2023-06-14 16:05:43 +1000

    Added spacebar thing

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -11,13 +11,14 @@ public class PlayerCharacter : MonoBehaviour
     [SerializeField] private int experience =   0;
 
     // Methods will go  underneath here
+    // we need our player character to attack when you hit the space bar
 
     // Start is called before the first frame update
     private void Start()
     {
         attack = 10;
         health = 100;
-        level = 0;
+        level = 1;
         experience = 0;
         StartTheGameAtLevel1();
     }
@@ -25,14 +26,20 @@ public class PlayerCharacter : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        
+        // if the player presses the spacebar
+        if (Input.GetKeyDown(KeyCode.Space)) ;
+        {
+            // ..... do a thing
+            Debug.Log("Space bar has been destroyed!");
+            level++;
+        }
     }
 
     private void StartTheGameAtLevel1()
     {
         attack = 10;
         health = 100;
-        level = 0;
+        level = 1;
         experience = 0;
     }
 }
