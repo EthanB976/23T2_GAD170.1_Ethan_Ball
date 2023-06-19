0c4b50d92f6a711c297c63adc91bcbdcd418e434
EthanB976 <136556095+EthanB976@users.noreply.github.com>
2023-06-19 16:26:15 +1000

    added enemy, added new player stuff (attackmultiplier etc)

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private double health;
+    [SerializeField] private int level;
+    //private GameObject collision;
+    //private Vector3 RandomPos;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Newenemy();
+        //level = Random.Range(1, 6);
+        //health = 5 + 5 * level;
+        //collision = GetComponent<GameObject>();
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        //OnDeath();
     }
+
+    private void OnDeath()
+    {
+        Newenemy();
+        //RandomPos.x = Random.Range(-3, 4);
+        //RandomPos.y = Random.Range(-3, 4);
+        //RandomPos.z = 0;
+        //Instantiate(Transform.transform,RandomPos,Quaternion.identity);
+    }
+    private void takeDamage(double damage)
+    {
+        health -= damage;
+        Debug.Log("Enemy took " + damage + " damage");
+        if (health <= 0)
+        {
+            OnDeath();
+        }
+    }
+    private void Newenemy()
+    {
+        level = Random.Range(1, 6);
+        health = 5 + 5 * level;
+        Debug.Log("A new challenger appears!");
+    }
+
+
+
+
+
+
 }
+
+
+
--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour
 {
     //Variables go first
-    [SerializeField] private int attack     =  10;
-    [SerializeField] private int health     = 100;
-    [SerializeField] private int level      =   1;
-    [SerializeField] private int experience =   0;
+    [SerializeField] private double attack = 10;
+    [SerializeField] private int health = 100;
+    [SerializeField] private int level = 1;
+    [SerializeField] private int experience = 0;
+    [SerializeField] private double exptolevelup = 50;
+    [SerializeField] private GameObject zahenemy;
 
     // Methods will go  underneath here
     // we need our player character to attack when you hit the space bar
@@ -16,30 +19,47 @@ public class PlayerCharacter : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        attack = 10;
-        health = 100;
-        level = 1;
-        experience = 0;
-        StartTheGameAtLevel1();
+
     }
 
     // Update is called once per frame
     private void Update()
     {
         // if the player presses the spacebar
-        if (Input.GetKeyDown(KeyCode.Space)) ;
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             // ..... do a thing
             Debug.Log("Space bar has been destroyed!");
-            level++;
+            //LevelUp();
+
+        }
+        if (experience >= exptolevelup)
+        {
+            LevelUp();
+        }
+        if (level == 5)
+        {
+            //player wins
+            Debug.Log("You Win!!!!!!!!");
         }
     }
 
-    private void StartTheGameAtLevel1()
+    private void LevelUp()
     {
-        attack = 10;
-        health = 100;
-        level = 1;
-        experience = 0;
+        attack *= 1.225;
+        level++;
+        exptolevelup *= 1.75;
+        Debug.Log("Level up");
+        Debug.Log("Attack: " + Mathf.Round((float)attack * 100) / 100);
     }
+    /**
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     */
 }
+
